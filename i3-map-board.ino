4cacc7bd1b0e2fdf68e15edc5fb346b998df340d
mkfink <mkfink@gmail.com>
2017-10-01 20:24:17 -0400

    much faster, LWT works

--- i3-map-board.ino
+++ i3-map-board.ino
@@ -15,11 +15,11 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 // define some colors for the neopixels
-uint32_t red = pixels.Color(200,0,0);
-uint32_t blue = pixels.Color(0,0,200);
-uint32_t purple = pixels.Color(200,0,150);
-uint32_t green = pixels.Color(0,200,0);
-uint32_t yellow = pixels.Color(200,200,0);
+uint32_t red = pixels.Color(15,0,0);
+uint32_t blue = pixels.Color(0,0,15);
+uint32_t purple = pixels.Color(15,0,10);
+uint32_t green = pixels.Color(0,15,0);
+uint32_t yellow = pixels.Color(15,15,0);
 uint32_t off = pixels.Color(0,0,0);
 
 int delayVal = 50;
@@ -28,7 +28,8 @@ int i = 0;
 int j = 0;
 int k = 0;
 
-char buf[1024];
+char bufA[1024];
+char bufB[1024];
 const char* host_name = "i3_map_board";
 const char* ssid = "i3detroit-wpa";
 const char* password = "i3detroit";
@@ -38,49 +39,66 @@ const int mqtt_port = 1883;
 int numLights = ARRAY_SIZE(lights);
 
 void callback(char* topic, byte* payload, unsigned int length, PubSubClient *client) {
-  Serial.println("Message arrived [");
-  Serial.println(topic);
-  Serial.println("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+  // Serial.println("Message arrived [");
+  // Serial.println(topic);
+  // Serial.println("] ");
+  // for (int i = 0; i < length; i++) {
+    // Serial.print((char)payload[i]);
+  // }
+  // Serial.println();
   uint32_t c;
   for (int j = 0; j < numLights; j++) {
-  	Serial.println(topic);
-  	Serial.println(lights[j]->mqttTopic);
-  	sprintf(buf,"stat%s",lights[j]->mqttTopic);
-  	if (strcmp(topic, buf) == 0){
-  		Serial.print("yay");
+  	// Serial.println(topic);
+  	// Serial.println(lights[j]->mqttTopic);
+  	sprintf(bufA,"stat%sPOWER",lights[j]->mqttTopic);
+    sprintf(bufB,"tele%sLWT",lights[j]->mqttTopic);
+  	if (strcmp(topic, bufA) == 0){
+  		// Serial.print("yay");
 		  if((char)payload[1] == 'N') {
 		    lights[j]->state = ON;
 		  } else if ((char)payload[1] == 'F') {
 		    lights[j]->state = OFF;
 		  } else {
-		    Serial.println("NOT A THING FUCK");
+		    // Serial.println("NOT A THING FUCK");
 		    lights[j]->state = DISCONNECTED;
 		  }
 		}
+    else if (strcmp(topic, bufB) == 0){
+      // Serial.print("yay");
+      if((char)payload[1] == 'n') {
+        lights[j]->state = ON;
+      } else if ((char)payload[1] == 'f') {
+        lights[j]->state = DISCONNECTED;
+      } else {
+        // Serial.println("NOT A THING FUCK");
+        lights[j]->state = UNKNOWN;
+      }
+    }
 	}
 }
 
 void connectSuccess(PubSubClient* client, char* ip) {
-  Serial.println("win");
+  //Serial.println("win");
   //subscribe and shit here
-  sprintf(buf, "{\"Hostname\":\"%s\", \"IPaddress\":\"%s\"}", host_name, ip);
-  client->publish("tele/i3/inside/commons/map-board/INFO2", buf);
-  for (int j = 0; j < numLights; j++) {
-  	sprintf(buf,"stat%s",lights[j]->mqttTopic);
-		client->subscribe(buf);
-		sprintf(buf,"cmnd%s",lights[j]->mqttTopic);
-		client->publish(buf, "");
-		loop_mqtt();
-  }
+  sprintf(bufA, "{\"Hostname\":\"%s\", \"IPaddress\":\"%s\"}", host_name, ip);
+  client->publish("tele/i3/inside/commons/map-board/INFO2", bufA);
+  // for (int j = 0; j < numLights; j++) {
+  // 	sprintf(bufA,"stat%sPOWER",lights[j]->mqttTopic);
+		// client->subscribe(bufA);
+  //   sprintf(bufA,"tele%sLWT",lights[j]->mqttTopic);
+  //   client->subscribe(bufA);
+		// sprintf(bufA,"cmnd%sPOWER",lights[j]->mqttTopic);
+		// client->publish(bufA, " ");
+		// loop_mqtt();
+  // }
+  client->subscribe("stat/i3/commons/lights/+/POWER");
+  client->subscribe("tele/i3/commons/lights/+/LWT");
+  client->publish("cmnd/i3/commons/lights/all/POWER", " ");
 }
 
 void setup() {
   //start serial connection
-  Serial.begin(115200);
+  //Serial.begin(115200);
   setup_mqtt(connectedLoop, callback, connectSuccess, ssid, password, mqtt_server, mqtt_port, host_name);
   pixels.begin(); // This initializes the NeoPixel library.
   for (int j=0; j < NUM_PIXELS; j++) {
