1f3fa276a1e74f9ca853ba3173b1138e18a61d1d
mkfink <mkfink@gmail.com>
2017-11-07 22:18:17 -0500

    debugging

--- python/i3_map_board.py
+++ python/i3_map_board.py
@@ -235,10 +235,12 @@ def on_connect(client, userdata, flags, rc):
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
 	for item in deviceList:
+		print("subscribing to "+item['topic'])
 		client.subscribe(item['topic'])
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+	print("received"+msg.topic+" "+msg.payload)
 	for device in deviceList:
 		# Match message topic to device, determine state of device
 		# Coarse filtering for stat/tele
